40b6541bd0a934ff968d8aa2b1f89d367820f151
Charlie Root <root@test.data.deic.dk>
2015-03-05 19:53:23 +0100

    Added web interface for the admin (modify the mail subject , sender and url in the mail text).

---
+++ js/settings.js
@@ -0,0 +1,26 @@
+function add_settings(subject, sender, url){
+	$.ajax(OC.linkTo('user_group_admin','ajax/settingactions.php'), {
+		 type:'POST',
+		  data:{
+			 'action': 'addsubject', 'mailsubject': subject, 'mailsender': sender, 'accepturl': url
+		 },
+		 dataType:'json',
+		 success: function(data){
+
+		 },
+		error:function(data){
+			alert("Unexpected error!");
+		}
+	});
+}
+
+
+
+$(document).ready(function() {
+ 	$('#mailsubmit').click(function() {
+		subject = $('#mailsubject').val();
+		sender = $('#mailsender').val(); 
+		url = $('#accepturl').val();
+		add_settings(subject, sender, url );
+	});
+});		
