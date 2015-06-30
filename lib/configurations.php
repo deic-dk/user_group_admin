5b56ae64605a9c7c52098c8782ddf06d6d1200b7
psyllost <psyllost@gmail.com>
2015-06-30 17:01:20 +0200

    Delete configurations.php
    unnecessary

--- lib/configurations.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-\OCP\Config::setAppValue('user_group_admin', 'subject', 'Group Invitation to Data DeiC');
-\OCP\Config::setAppValue('user_group_admin', 'sender', 'cloud@data.deic.dk');
-\OCP\Config::setAppValue('user_group_admin', 'appurl', 'https://test.data.deic.dk/index.php/apps/user_group_admin?code=');
-
