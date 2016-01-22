65c55e28fd371a20b7b547e2dd798f3813143b52
ioanna <psyllost@gmail.com>
2016-01-22 15:45:10 +0100

    bug fix for invite notifications and streaming settings

--- lib/activityhooks.php
+++ lib/activityhooks.php
@@ -1,19 +1,16 @@
 <?php
 use \OCP\Util;
-
 class OC_User_Group_Hooks {
 	public static function register() {
 		\OCP\Util::connectHook('OC_Activity', 'post_create', 'OC_User_Group_Hooks', 'groupCreate');
 		\OCP\Util::connectHook('OC_Activity', 'delete', 'OC_User_Group_Hooks', 'groupDelete');
 		\OCP\Util::connectHook('OC_Activity', 'post_shared', 'OC_User_Group_Hooks', 'groupShare');
-
 		// hooking up the activity manager
 		$am = \OC::$server->getActivityManager();
 		$am->registerConsumer(function() {
 			return new Consumer();
 		});
 	}
-
 	public static function groupCreate($group, $uid) {
 		$params = array($group, $uid);
 		OC_User_Group_Hooks::addNotificationsForGroupAction($params, 'group', 'created_self', 'created_by');		
@@ -24,7 +21,8 @@ class OC_User_Group_Hooks {
 	}
 	public static function dbGroupShare($group, $uid, $owner) {
 		$params = array($group, $uid, $owner);
-                OC_User_Group_Hooks::addNotificationsForGroupAction($params, 'group', 'shared_user_self', 'shared_with_by');
+                OC_User_Group_Hooks::addNotificationsForGroupAction($params, 'group', 'shared_user_self', 'shared_user_self');
+		OC_User_Group_Hooks::addNotificationsForGroupAction($params, 'group', 'shared_with_by', 'shared_with_by');
 	}
 	public static function groupShare($group, $uid, $owner) {
                 if(!\OCP\App::isEnabled('files_sharding')){
@@ -49,30 +47,37 @@ class OC_User_Group_Hooks {
 			$auser = $group[1];
 		}
 		$userSubject = $subject;
-		$filteredStreamUsers = \OCA\Activity\UserSettings::filterUsersBySetting(array($user), 'stream', 'group');
-                $filteredEmailUsers = \OCA\Activity\UserSettings::filterUsersBySetting(array($user), 'email', 'group');
-		foreach (array($user) as $user) {
-
+		$filteredStreamUsers = \OCA\Activity\UserSettings::filterUsersBySetting(array($user, $auser), 'stream', 'group');
+                $filteredEmailUsers = \OCA\Activity\UserSettings::filterUsersBySetting(array($user, $auser), 'email', 'group');
+//		foreach (array($user) as $user) {
 		//if (!empty($filteredStreamUsers) && !empty($filteredEmailUsers)) {
+		if ($userSubject == "shared_with_by") {
 			 OC_User_Group_Hooks::addNotificationsForUser(
                                 $user, $auser, $userSubject,
                                 $group, true,
-				!empty($filteredStreamUsers[$user]),
-				!empty($filteredEmailUsers[$user]) ? $filteredEmailUsers[$user] : 0,
+				!empty($filteredStreamUsers[$auser]),
+				!empty($filteredEmailUsers[$auser]) ? $filteredEmailUsers[$auser] : 0,
                                 40, $activityType
                         );
-
-		} 	
+	//	} 	
+		} else {
+			OC_User_Group_Hooks::addNotificationsForUser(
+                                $user, $auser, $userSubject,
+                                $group, true,
+                                !empty($filteredStreamUsers[$user]),
+                                !empty($filteredEmailUsers[$user]) ? $filteredEmailUsers[$user] : 0,
+                                40, $activityType
+                        );
+		}
 	}
-
 	protected static function addNotificationsForUser($user, $auser, $subject, $path, $isFile, $streamSetting, $emailSetting, $priority , $type ) {
 		$link = "/index.php/apps/user_group_admin"; 
 	        $app = 'user_group_admin';	
 		if ($streamSetting) {
 			if ($subject == 'shared_user_self') {
 				self::send($app, $subject, $path, '', array(), '', $link, $user, $user, $type, 40);
-				self::send($app, 'shared_with_by', array($path[0],$user), '', array(), '', $link, $user, $auser, $type, 40); 
-
+			}else if ($subject == 'shared_with_by') {
+				self::send($app, $subject, array($path[0],$user), '', array(), '', $link, $user, $auser, $type, 40); 
 			}else if ($subject == 'deleted_self') {
 				self::send($app, $subject, array($path[0]), '', array(), '', $link, $user, $auser,$type, 40); 
         	 	}else if ($subject == 'deleted_by'){
@@ -98,23 +103,15 @@ class OC_User_Group_Hooks {
 			}
 		}		 
 	}
-
 	public static function send($app, $subject, $subjectparams = array(), $message = '', $messageparams = array(), $file = '', $link = '', $user = '', $affecteduser = '', $type = '', $prio = Data::PRIORITY_MEDIUM) {
 		$timestamp = time();
-
 		// store in DB
 		$query = \OCP\DB::prepare('INSERT INTO `*PREFIX*activity`(`app`, `subject`, `subjectparams`, `message`, `messageparams`, `file`, `link`, `user`, `affecteduser`, `timestamp`, `priority`, `type`)' . ' VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )');
 		$query->execute(array($app, $subject, serialize($subjectparams), $message, serialize($messageparams), $file, '', $user, $affecteduser, $timestamp, $prio, $type));
-
 		// fire a hook so that other apps like notification systems can connect
 		\OCP\Util::emitHook('OC_Activity', 'post_event', array('app' => $app, 'subject' => $subject, 'user' => $user, 'affecteduser' => $affecteduser, 'message' => $message, 'file' => $file, 'link'=> '', 'prio' => $prio, 'type' => $type));
-
 		return true;
 	}
-
-
-
-
 }
 
 
