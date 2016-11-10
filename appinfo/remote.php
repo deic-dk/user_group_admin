209172e32d5da51db53d4f9cb5ae59a5e708cb73
deltafunction <frederik@orellana.dk>
2016-11-10 12:50:37 +0100

    webroot

--- appinfo/remote.php
+++ appinfo/remote.php
@@ -6,7 +6,7 @@ OCP\App::checkAppEnabled('user_group_admin');
 //$group = preg_replace("/^\/remote.php\/groupdirs\/([^\/]*)\/*/", "$1", $_SERVER['REQUEST_URI']);
 $groupDir = '/'.$_SERVER['PHP_AUTH_USER'].'/user_group_admin/';
 $_SERVER['BASE_DIR'] = $groupDir;
-$_SERVER['BASE_URI'] = "/remote.php/groupdirs";
+$_SERVER['BASE_URI'] = OC::$WEBROOT."/remote.php/groupdirs";
 
 include('chooser/appinfo/remote.php');
 
