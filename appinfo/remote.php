b93dc70327091417587b558f304f5d11151961e9
deltafunction <frederik@orellana.dk>
2018-06-25 14:37:59 +0200

    Access specific group dirs

--- appinfo/remote.php
+++ appinfo/remote.php
@@ -3,10 +3,25 @@
 OCP\App::checkAppEnabled('chooser');
 OCP\App::checkAppEnabled('user_group_admin');
 
-//$group = preg_replace("/^\/remote.php\/groupdirs\/([^\/]*)\/*/", "$1", $_SERVER['REQUEST_URI']);
-$groupDir = '/'.$_SERVER['PHP_AUTH_USER'].'/user_group_admin/';
+$group = "";
+
+if(preg_match("|^".OC::$WEBROOT."/group/|", $_SERVER['REQUEST_URI'])){
+	$group = preg_replace("|^".OC::$WEBROOT."/group/|", "", $_SERVER['REQUEST_URI']);
+	$_SERVER['BASE_URI'] = OC::$WEBROOT."/group";
+}
+elseif(preg_match("|^".OC::$WEBROOT."/remote.php/group/|", $_SERVER['REQUEST_URI'])){
+	$group = preg_replace("|^".OC::$WEBROOT."/remote.php/group/|", "", $_SERVER['REQUEST_URI']);
+	$_SERVER['BASE_URI'] = OC::$WEBROOT."/remote.php/group";
+}
+
+OC_Log::write('chooser','Group dir access: '.$group.':'.\OCP\USER::getUser(), OC_Log::WARN);
+
+$group = preg_replace("|/.*$|", "", $group);
+$group = urldecode($group);
+
+$groupDir = '/'.$_SERVER['PHP_AUTH_USER'].'/user_group_admin/'.$group;
+
 $_SERVER['BASE_DIR'] = $groupDir;
-$_SERVER['BASE_URI'] = OC::$WEBROOT."/remote.php/groupdirs";
 
 include('chooser/appinfo/remote.php');
 
